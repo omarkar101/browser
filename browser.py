ba00968229edd2580c58353f73ca0c03f3c00486
Omar Karazoun <59054172+omarkar101@users.noreply.github.com>
2024-04-02 00:55:22 +0300

    continue

--- browser.py
+++ browser.py
@@ -55,10 +55,28 @@ def show(body):
       elif not in_tag:
           print(c, end="")
   
-def load(url):
+
+import tkinter
+WIDTH, HEIGHT = 800, 600
+class Browser:
+  def __init__(self):
+    self.window = tkinter.Tk()
+    self.canvas = tkinter.Canvas(
+      self.window,
+      width=WIDTH,
+      height=HEIGHT
+    )
+    self.canvas.pack()
+
+  def load(self, url):
     body = url.request()
     show(body)
+    self.canvas.create_rectangle(10, 20, 400, 300)
+    self.canvas.create_oval(100, 100, 150, 150)
+    self.canvas.create_text(200, 150, text="Hi!")
+
 
 if __name__ == "__main__":
   import sys
-  load(URL(sys.argv[1]))
+  Browser().load(URL(sys.argv[1]))
+  tkinter.mainloop()
